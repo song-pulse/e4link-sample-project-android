84a27703adb7056f4edf55eb3729ebf94b4e5226
Anja Koller <anja.koller@uzh.ch>
2020-07-02 14:40:30 +0200

    Add participant ID to Overview Page

--- app/src/main/java/com/empatica/sample/OverviewActivity.java
+++ app/src/main/java/com/empatica/sample/OverviewActivity.java
@@ -2,6 +2,7 @@ package com.empatica.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OverviewActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class OverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+        TextView participant_id;
+        participant_id = (TextView) findViewById(R.id.participant_id);
+        displayPID(participant_id, "Participant ID: " + EntryActivity.pId);
+    }
+
+    private void displayPID(final TextView label, final String text) {
+        runOnUiThread(() -> label.setText(text));
+
     }
 }

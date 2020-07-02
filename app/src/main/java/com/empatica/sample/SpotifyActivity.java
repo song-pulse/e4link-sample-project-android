776d05147e8f06b2ae3ac8e8b8c17bc7acfb7668
Anja Koller <anja.koller@uzh.ch>
2020-07-02 11:38:13 +0200

    Fix webview rendering

--- app/src/main/java/com/empatica/sample/SpotifyActivity.java
+++ app/src/main/java/com/empatica/sample/SpotifyActivity.java
@@ -16,6 +16,7 @@ public class SpotifyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotify);
         WebView myWebView = findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new MyBrowser());
         String spotifyUrl = "http://130.60.24.99:8080/spotify/authorize";
         myWebView.loadUrl(spotifyUrl);
@@ -28,13 +29,9 @@ public class SpotifyActivity extends AppCompatActivity {
             Log.d("spotifyurl", "Url"+url);
 
             if(url.contains("whoami")) {
-                //onPageFinished(view, url);
                 cookie = CookieManager.getInstance().getCookie(url);
                 Log.d("cookieSpotify", "All the cookies in a string:" + cookie);
-                // only do this if url is callback
-                if(url.contains("whoami")) {
-                    SpotifyActivity.this.finish(); // finish spotifyActivity
-                }
+                SpotifyActivity.this.finish(); // finish spotifyActivity
             }
             return false;
         }

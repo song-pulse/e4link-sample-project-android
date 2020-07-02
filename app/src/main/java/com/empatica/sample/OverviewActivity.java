b1e43d5af38a3098b502cd2bd1bfb48f63eae02a
Anja Koller <anja.koller@uzh.ch>
2020-07-02 14:45:40 +0200

    Add example graph

--- app/src/main/java/com/empatica/sample/OverviewActivity.java
+++ app/src/main/java/com/empatica/sample/OverviewActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class OverviewActivity extends AppCompatActivity {
 
     @Override
@@ -13,10 +17,23 @@ public class OverviewActivity extends AppCompatActivity {
         TextView participant_id;
         participant_id = (TextView) findViewById(R.id.participant_id);
         displayPID(participant_id, "Participant ID: " + EntryActivity.pId);
+        displayGraph();
     }
 
     private void displayPID(final TextView label, final String text) {
         runOnUiThread(() -> label.setText(text));
 
     }
+
+    public void displayGraph() {
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        graph.addSeries(series);
+    }
+
+
 }
